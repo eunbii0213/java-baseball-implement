bf47878e5c896c3d2fdf602827013ec89041fa52
eunbii0213 <eunbii0213@naver.com>
2022-03-10 01:08:12 +0900

    기능1 구현 소스코드 입니다. 추가로 기능2,3 을 추가하게되며 코드가 달라질 수 있을 것 같습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,92 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+
+
 public class Application {
+    static boolean[] visit; //3개의 수는 중복될 수 없으므로 방문여부를 확인하기위한 배열 선언입니다.
+    static ArrayList<Integer> randomNumber; //난수를 저장하는 list입니다.
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+
+        // 난수 세 개 입력받기 : 1~9의 값을 랜덤으로(중복을 허용하지 않고) 고릅니다.
+        randomNumber = new ArrayList<Integer>(3);
+        visit = new boolean[10];
+
+        getRandomNum(); //난수를 입력받습니다.
+        for(int i=0;i<3;i++){
+            System.out.println(randomNumber.get(i));
+        }
+
+        //유저가 숫자를 맞춰봅니다.
+        UserGuessNumber();
+
+
+    }
+    public static void UserGuessNumber(){
+
+        while(true){
+            int strike=0, ball=0;
+
+            System.out.print("숫자를 입력해주세요 : ");
+            int userNumber = Integer.parseInt(Console.readLine());
+            ArrayList<Integer> userNumberList = new ArrayList<Integer>(3);
+
+            //입력받은 수를 백의자리, 십의자리, 일의자리로 나누어 저장합니다.
+            userNumberList.add(userNumber/100);
+            userNumberList.add((userNumber%100)/10);
+            userNumberList.add((userNumber%100)%10);
+
+            //검사작업을 시작합니다.
+            for(int i=0;i<3;i++){
+                if(randomNumber.get(i).equals(userNumberList.get(i))){
+                    strike++;
+                }else if(userNumberList.contains(randomNumber.get(i))){
+                    ball++;
+                }
+            }
+
+            //결과를 프린트합니다.
+            printResult(ball, strike);
+
+            ball = 0;
+            strike = 0;
+        }
+    }
+
+    public static void printResult(int ball, int strike){
+        if(ball!=0 && strike!=0){
+            System.out.println(ball+"볼"+" "+strike+"스트라이크");
+        }else if(ball==0 && strike!=0){
+            System.out.println(strike+"스트라이크");
+        } else if(ball!=0 && strike==0){
+            System.out.println(ball+"볼");
+        }else{
+            System.out.println("낫싱");
+        }
+    }
+
+    public static void initVisit(){
+        //visit배열을 초기화. (게임 재시작 하는 경우를 대비)
+        for(int i=0; i<visit.length;i++){
+            visit[i]=false;
+        }
+
+    }
+    public static void getRandomNum(){
+
+        //난수입력받음(숫자가 중복되지 않기위해 visit여부를 체크하며 넣는다)
+        for(int i=0;i<3;i++){
+            int number = Randoms.pickNumberInRange(1,9);
+            if(!visit[number]){
+                visit[number]=true;
+                randomNumber.add(number);
+            }else {
+                i--;
+            }
+        }
+
     }
 }
