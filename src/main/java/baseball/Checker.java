ea984d87898ab0283e8c1377db949c1ac13c6a16
eunbii0213 <eunbii0213@naver.com>
2022-03-16 15:29:26 +0900

    (03/15) 반복문 변수 i-> index로 수정했습니다.

--- src/main/java/baseball/Checker.java
+++ src/main/java/baseball/Checker.java
@@ -2,17 +2,20 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Checker {
+import java.util.ArrayList;
 
-    private boolean[] visit; //유저입력에서의 중복을 체크하는 배열입니다.
+public class Checker {
+    private ArrayList<Boolean> visit;
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TEN = 10;
+    public static final char CHAR_TO_INT ='0';
 
     Checker() {
-        visit = new boolean[10]; // 0,1,2,3,4,5,6,7,8,9
+        visit = new ArrayList<>(TEN);
     }
 
-    //User의 입력을 검사하고 유효하지 않으면 Illegal Exception을 발생시킵니다.
     public void userInputChecker(String userNumber, int length) {
-
         char[] userNumberToCharArray = userNumber.toCharArray();
 
         try {
@@ -25,50 +28,43 @@ public class Checker {
             throw new IllegalArgumentException("올바른 길이의 숫자를 입력해주세요.");
         }
 
-        for (int index = 0; index < length; index++) {
-            if (userNumberToCharArray[index] - '0' == 0) {
+        for (int index = ZERO; index < length; index++) {
+            if (userNumberToCharArray[index] - CHAR_TO_INT == ZERO) {
                 throw new IllegalArgumentException("1~9 사이의 숫자만 입력가능합니다.");
             }
         }
 
-        //reOrEnd 입력 받을 때는 숫자중복을 검사하지않습니다.
-        if (length != 1) {
+        if (length != ONE) {
             if (isUserInputSameNumber(userNumberToCharArray)) {
                 throw new IllegalArgumentException("중복되는 숫자는 입력할 수 없습니다.");
             }
         }
     }
 
-    //reOrEnd 입력을 받을 때 체크합니다.
     public int reOrEndUserInput() {
-
         String userInput = Console.readLine();
 
         try {
             userInputChecker(userInput, 1);
-
-            int userInputToInt = Integer.parseInt(userInput);
-            return userInputToInt;
+            return Integer.parseInt(userInput);
 
         } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("1혹은 2를 입력해주세요");
         }
     }
 
-    //유저가 111, 222같은 중복되는 숫자 입력을 방지합니다.
     public boolean isUserInputSameNumber(char[] userNumberToCharArray) {
-
-        int index = 0;
+        int index = ZERO;
         initialVisitArr();
 
-        for (int visitCheckIndex = 0; visitCheckIndex < visit.length; visitCheckIndex++) {
-            int target = userNumberToCharArray[index] - '0';
+        for (int visitCheckIndex = ZERO; visitCheckIndex < visit.size(); visitCheckIndex++) {
+            int target = userNumberToCharArray[index] - CHAR_TO_INT;
 
-            if (!visit[visitCheckIndex] && target == visitCheckIndex) {
-                visit[visitCheckIndex] = true;
+            if (!visit.get(visitCheckIndex) && target == visitCheckIndex) {
+                visit.set(visitCheckIndex, true);
                 index++;
-                visitCheckIndex = -1;
-            } else if (visit[visitCheckIndex] && target == visitCheckIndex) {
+                visitCheckIndex = -ONE;
+            } else if (visit.get(visitCheckIndex) && target == visitCheckIndex) {
                 return true;
             }
 
@@ -81,8 +77,10 @@ public class Checker {
     }
 
     public void initialVisitArr() {
-        for (int index = 0; index < 10; index++) {
-            visit[index] = false;
+        int index = ZERO;
+        for (boolean isTrue : visit) {
+            visit.set(index, false);
+            index++;
         }
     }
 }
